d4e76afaf3446f6915348f4cd525f69584c24654
marcelopajr <marcelo96jr@gmail.com>
2021-06-29 10:04:41 -0300

    Firebase implementado

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'modules/home/home_page.dart';
+import 'modules/login/login_page.dart';
 import 'shared/themes/app_colors.dart';
 
 class AppWidget extends StatelessWidget {
@@ -11,7 +12,7 @@ class AppWidget extends StatelessWidget {
       theme: ThemeData(
         primaryColor: AppColors.primary,
       ),
-      home: HomePage(),
+      home: LoginPage(),
     );
   }
 }
