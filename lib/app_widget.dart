8460b5e6b3ef151559a3185959b1a0fc345fec7f
marcelopajr <marcelo96jr@gmail.com>
2021-07-01 16:17:07 -0300

    BarcodeScanner implementado

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'modules/barcode_scanner/barcode_scanner_page.dart';
 import 'modules/home/home_page.dart';
+import 'modules/insert_boleto/insert_boleto_page.dart';
 import 'modules/login/login_page.dart';
 import 'modules/splash/splash_page.dart';
 import 'shared/themes/app_colors.dart';
 
 class AppWidget extends StatelessWidget {
+  AppWidget() {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.portraitUp,
+    ]);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -21,6 +30,7 @@ class AppWidget extends StatelessWidget {
         "/home": (context) => HomePage(),
         "/login": (context) => LoginPage(),
         "/barcode_scanner": (context) => BarcodeScannerPage(),
+        "/insert_boleto": (context) => InsertBoletoPage(),
       },
     );
   }
